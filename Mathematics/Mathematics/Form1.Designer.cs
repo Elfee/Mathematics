964bfe1294e312e22029c1dc862033979bc9dec1
ElenaBr <bragnikova08@yandex.ru>
2015-03-13 22:25:11 +0300

    Заливаю остальные файлы

---
+++ Mathematics/Mathematics/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GameForBaby2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 65F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(-10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 98);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 53F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.Navy;
+            this.textBox1.Location = new System.Drawing.Point(295, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 87);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(295, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(488, 351);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Математика";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ Mathematics/Mathematics/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GameForBaby2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Primer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int otvet;
+            int.TryParse(textBox1.Text,out otvet);
+            if (c == 0)
+            {
+                if (a + b == otvet)
+                {
+                    MessageBox.Show("Молодец!Чмоки-Чмоки!");
+                    Primer();
+                }
+                else
+                {
+                    MessageBox.Show("Неправильно(");
+                }
+            }
+            else
+            {
+                if (a - b == otvet)
+                {
+                    MessageBox.Show("Молодец!");
+                    Primer();
+                }
+                else
+                {
+                    MessageBox.Show("Неправильно(");
+                }
+            }
+
+            textBox1.Text = "";
+        }
+        public void Primer()
+        {
+            Random r = new Random();
+            a = r.Next(10);
+            b = r.Next(10);
+            c = r.Next(0,2);
+            if (c == 0)
+            {
+                label1.Text = a + "+" + b;
+            }
+            else
+            {
+                a = r.Next(10);
+                b = r.Next(a);
+                label1.Text = a + "-" + b;
+            }
+        }
+
+    }
+}
